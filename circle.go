a15ce35ccd8b2ff4107b4181679b0adb2d739259
tarugatto <talgat.zhaimenov@gmail.com>
2022-06-01 16:55:33 +0600

    Modified3. Finished 'shapes' task

--- circle.go
+++ circle.go
@@ -7,10 +7,10 @@ type Circle struct {
 	Radius float64
 }
 
-func (с Circle) CalcArea() float64 {
+func (c Circle) CalcArea() float64 {
 	return math.Pi * (c.Radius * c.Radius)
 }
 
-func (с Circle) CalcPerimeter() float64 {
+func (c Circle) CalcPerimeter() float64 {
 	return 2 * math.Pi * c.Radius
 }
