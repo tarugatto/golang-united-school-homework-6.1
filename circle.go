e6b7768b1fe7b962a97d16583af98fd616198a39
tarugatto <talgat.zhaimenov@gmail.com>
2022-06-01 16:41:25 +0600

    Modified1. Finished 'shapes' task

--- circle.go
+++ circle.go
@@ -1,6 +1,16 @@
 package golang_united_school_homework
 
+import "math"
+
 // Circle must satisfy to Shape interface
 type Circle struct {
 	Radius float64
+}
+
+func (с Circle) CalcArea() float64 {
+	return math.Pi * (c.Radius * c.Radius)
+}
+
+func (с Circle) CalcPerimeter() float64 {
+	return 2 * math.Pi * c.Radius
 }
